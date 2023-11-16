d21ff8633fa7f96008b51ff8b64e812c43676854
Nin <snin934@gmail.com>
2023-11-16 08:14:09 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import { useLayoutEffect } from "react"
-import LocomotiveScroll from 'locomotive-scroll'
+// import LocomotiveScroll from 'locomotive-scroll'
 
 export default function Home() {
 
-  const locomotiveScroll = new LocomotiveScroll();
+  useLayoutEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, [])
 
   return (
     <main className="contianer">
